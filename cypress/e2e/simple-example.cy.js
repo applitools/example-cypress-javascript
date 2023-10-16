c63083f67dc24cf9d278618188172bbd2742c0e5
Mattyj10 <5661826+MattyJ10@users.noreply.github.com>
2023-10-16 13:19:09 -0700

    added simpler example

---
+++ cypress/e2e/simple-example.cy.js
@@ -0,0 +1,31 @@
+describe('ACME Bank', () => {
+    beforeEach(() => {
+		// Start Applitools Visual AI Test
+        cy.eyesOpen({
+            appName: 'ACME Bank',
+            testName: Cypress.currentTest.title,
+        })
+    })
+    it('should log into a bank account', () => {
+        cy.visit('https://sandbox.applitools.com/bank')
+		
+		// Full Page - Visual AI Assertion
+        cy.eyesCheckWindow({
+            tag: "Login page",
+            fully: true
+        });
+
+        cy.get('#username').type('andy')
+        cy.get('#password').type('i<3pandas')
+        cy.get('#log-in').click()
+
+        cy.eyesCheckWindow({
+            tag: "Main page",
+            fully: true,
+        });
+    })
+    afterEach(() => {
+		// End Applitools Visual AI Test
+        cy.eyesClose()
+    })
+})
